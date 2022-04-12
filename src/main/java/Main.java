67d6ab54aa567020bf24576526c8104919f571a9
stein <55532350+steinhs@users.noreply.github.com>
2022-04-12 20:31:53 +0200

    Sequential method added. Printout for Seq, par, speedup and efficiecy added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,40 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         int nCores = Runtime.getRuntime().availableProcessors();
-        long startTime, endTime, seqTotalTime, parTotalTime;
+        long startTime, endTime;
+        double totalTimeSeq, totalTimePar;
 
         int[] array = new ArrayReader().readFileToArray("testcase1.txt");
-        System.out.println("Numbers read into array: " + array.length);
+        System.out.println("\nNumbers read into array: " + array.length);
 
-        int[] result = new Sequential(array).findMinMax();
-        System.out.println("Result of Sequential\n- Min: " + result[0] + "\n- Max: " + result[1]);
+        startTime = System.nanoTime();
+        int[] resultSeq = new Sequential(array).findMinMax();
+        endTime = System.nanoTime();
+        totalTimeSeq = (double) (endTime-startTime)/1000000;
 
+
+        startTime = System.nanoTime();
+        // PARALLEL METHOD
+        endTime = System.nanoTime();
+        totalTimePar = (double) (endTime-startTime)/1000000;
+
+
+
+
+
+        double speedup = totalTimeSeq/totalTimePar;
+        double efficiency = speedup/nCores;
+
+        System.out.println("\n- - SEQUENTIAL - -");
+        System.out.println("Execution time: " + totalTimeSeq + " milliseconds");
+        System.out.println("Result\n- Min: " + resultSeq[0] + "\n- Max: " + resultSeq[1]);
+
+        System.out.println("\n- - PARALLEL - -");
+        System.out.println("Execution time: " + totalTimePar + " milliseconds");
+        System.out.println("Result\n- Min: " + 0 + "\n- Max: " + 0);
+
+        System.out.println("\n- - STATS - -");
+        System.out.println("- Speedup: " + speedup);
+        System.out.println("- Efficiency: " + efficiency);
     }
 }
